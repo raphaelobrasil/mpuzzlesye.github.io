e4e8afbd0ad50d7082c585012719a2068d23dd0f
Raphael Brasil <raphael.brasil@outlook.com>
2022-06-11 11:46:28 -0300

    feat: include header searching page

--- js/search.js
+++ js/search.js
@@ -11,30 +11,52 @@ const filterInterface = (title, description, value) =>
     || (type.toLowerCase()).includes(value.toLowerCase())
     )[0] !== undefined 
 
-const searching = async (value) => {
+const searching = (interface, functions, searchPage, mainPage, value) => {
+
+  console.log('puzzle Functions', functions.PuzzleTables.filter((({ functionMethod, input, output, description }) => filterFunctions(functionMethod, input, output, description, value))))
+  console.log('Modules Functions', functions.Modules.filter((({ functionMethod, input, output, description }) => filterFunctions(functionMethod, input, output, description, value))))
+  console.log('Match3 Functions', functions.Match3.filter((({ functionMethod, input, output, description }) => filterFunctions(functionMethod, input, output, description, value))))
+  console.log('GachaSystem Functions', functions.GachaSystem.filter((({ functionMethod, input, output, description }) => filterFunctions(functionMethod, input, output, description, value))))
+  
+  console.log('puzzle interface', interface.PuzzleTables.filter(({ title, description }) => filterInterface(title, description, value)))
+  console.log('modules interface', interface.Modules.filter(({ title, description }) => filterInterface(title, description, value)))
+  console.log('match3 interface', interface.Match3.filter(({ title, description }) => filterInterface(title, description, value)))
+
+}
+
+const openSearching = async (value) => {
   const searchingInterface = await jsonInterface
   const searchingFunctions = await jsonFunctions
   const contentSearch = document.querySelector('.contentSearch')
-  const contentPage = document.querySelector('.contentPage')
+  const contentPage = document.querySelector('.contentPage')  
+  const xhttp = new XMLHttpRequest()
 
   if (value !== '' && value !== undefined && value) {
-      contentSearch.style.display = 'flex'
-      contentPage.style.display = 'none'
-      console.log('puzzle Functions', searchingFunctions.PuzzleTables.filter((({ functionMethod, input, output, description }) => filterFunctions(functionMethod, input, output, description, value))))
-      console.log('Modules Functions', searchingFunctions.Modules.filter((({ functionMethod, input, output, description }) => filterFunctions(functionMethod, input, output, description, value))))
-      console.log('Match3 Functions', searchingFunctions.Match3.filter((({ functionMethod, input, output, description }) => filterFunctions(functionMethod, input, output, description, value))))
-      console.log('GachaSystem Functions', searchingFunctions.GachaSystem.filter((({ functionMethod, input, output, description }) => filterFunctions(functionMethod, input, output, description, value))))
-      
-      console.log('puzzle interface', searchingInterface.PuzzleTables.filter(({ title, description }) => filterInterface(title, description, value)))
-      console.log('modules interface', searchingInterface.Modules.filter(({ title, description }) => filterInterface(title, description, value)))
-      console.log('match3 interface', searchingInterface.Match3.filter(({ title, description }) => filterInterface(title, description, value)))
+    contentSearch.style.display = 'flex'
+    contentPage.style.display = 'none'
+
+    xhttp.onreadystatechange = async function() {
+      if (this.readyState == 4) {
+        if (this.status == 200) { 
+          contentSearch.innerHTML = this.responseText
+          searching(searchingInterface, searchingFunctions, contentSearch, contentPage, value)
+        }
+        if (this.status == 404) { contentSearch.innerHTML = `Page not found.` }
+      }
+    }
+
+    xhttp.open("GET", `./pages/search/search.html`, true)
+    xhttp.send()
+
   } else {
-      contentSearch.style.display = 'none'
-      contentPage.style.display = 'unset'
+    contentSearch.style.display = 'none'
+    contentPage.style.display = 'unset'
   }
 }
 
 const getSearch = (value) => {
-  searching(value).catch(er => console.log('Searching Error:', er))
+  openSearching(value).catch(er => console.log('Searching Error:', er))
 }
+
+getSearch('ra')
 
